3d87eac724423e8bc8ec0ee000c5654718c4ad6a
root <amuMabirimise>
2023-11-24 10:11:57 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
-# install puppet-lint -v 2.5.0
+# 2-install_puppet_lint.pp
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Exec resource to install puppet-lint
+exec { 'install_puppet_lint':
+  command => '/usr/bin/gem install puppet-lint -v 2.5.0',
+  path    => '/usr/bin',
+  unless  => '/usr/bin/gem list puppet-lint | grep -q 2.5.0',
 }
