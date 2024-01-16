a983a57c81306b8893b2c4a0a103ae1f4e56796f
root <amuMabirimise>
2024-01-16 10:38:41 -0800

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,44 @@
 #!/usr/bin/python3
-
+"""
+Contains the top_ten function
+"""
 import requests
 
 
 def top_ten(subreddit):
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    """prints the titles of the top ten hot posts for a given subreddit"""
+    url = f'http://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'User-Agent': 'Python/requests:APIproject:v1.0.0 (by /u/aaorrico23)'}
+    params = {'limit': 10}
+
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()  # Raise HTTPError for bad responses
+
         data = response.json()
-        posts = data['data']['children']
-        for post in posts:
-            title = post['data']['title']
-            print(title)
+        posts = data.get('data', {}).get('children', [])
+
+        if not posts:
+            print(None)
+        else:
+            for post in posts:
+                print(post.get('data', {}).get('title', None))
+
+    except requests.exceptions.HTTPError as e:
+        if response.status_code == 403:
+            print(f"Error: 403 Client Error: Blocked for url: {url}. Unable to fetch posts for subreddit '{subreddit}'.")
+        else:
+            print(f"Error: {e}. Unable to fetch posts for subreddit '{subreddit}'.")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Request Error: {e}")
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
     else:
-        print(None)
+        subreddit_name = sys.argv[1]
+        top_ten(subreddit_name)
