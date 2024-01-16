95fdba76996961c234d2d5d9859492a12d4541fb
root <amuMabirimise>
2024-01-16 10:57:15 -0800

    subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,48 +1,22 @@
 #!/usr/bin/python3
-"""Function of query subscribers on a given Reddit subreddit."""
-#!/usr/bin/python3
-"""Function of query subscribers on a given Reddit subreddit."""
-import requests
-import time
+"""
+    Requesting a Reddit API subreddits
+"""
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "CustomUserAgent/1.0"}
+    """Returning of the number of subscribers for a given subreddit"""
+    import requests
 
+    header = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 \
+                Safari/537.36 Edg/119.0.0.0"
+    }
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        time.sleep(2)
-
-        if response.status_code == 200:
-            data = response.json()
-            subscribers_count = data['data']['subscribers']
-            return subscribers_count
-
-        elif response.status_code == 302:
-            print(f"Redirect for subreddit '{subreddit}'. Invalid subreddit.")
-            return 0
-
-        elif response.status_code == 404:
-            print(f"Subreddit '{subreddit}' not found.")
-            return 0
-
-        else:
-            print(f"Error: {response.status_code}")
-            return 0
-
-    except requests.exceptions.RequestException as e:
-        print(f"Request Error: {e}")
+        response = requests.get(url, headers=header, allow_redirects=False)
+        subscribers = response.json().get('data').get('subscribers')
+        return subscribers
+    except Exception:
         return 0
-
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit_name = sys.argv[1]
-        subscribers_count = number_of_subscribers(subreddit_name)
-        print(f"{subreddit_name} has {subscribers_count} subscribers.")
