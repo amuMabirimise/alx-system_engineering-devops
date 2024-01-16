d761fd35e256720f25a03a0af466b013ba645294
root <amuMabirimise>
2024-01-16 10:45:30 -0800

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,44 +1,23 @@
 #!/usr/bin/python3
 """
-Contains the top_ten function
+    Request top ten posts of a subreddit from Reddit API
 """
-import requests
 
 
 def top_ten(subreddit):
-    """prints the titles of the top ten hot posts for a given subreddit"""
-    url = f'http://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'Python/requests:APIproject:v1.0.0 (by /u/aaorrico23)'}
-    params = {'limit': 10}
+    """Return the first 10 posts for a given subreddit"""
+    import requests
 
+    header = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 \
+                Safari/537.36 Edg/119.0.0.0"
+    }
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     try:
-        response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status()  # Raise HTTPError for bad responses
-
-        data = response.json()
-        posts = data.get('data', {}).get('children', [])
-
-        if not posts:
-            print(None)
-        else:
-            for post in posts:
-                print(post.get('data', {}).get('title', None))
-
-    except requests.exceptions.HTTPError as e:
-        if response.status_code == 403:
-            print(f"Error: 403 Client Error: Blocked for url: {url}. Unable to fetch posts for subreddit '{subreddit}'.")
-        else:
-            print(f"Error: {e}. Unable to fetch posts for subreddit '{subreddit}'.")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Request Error: {e}")
-
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit_name = sys.argv[1]
-        top_ten(subreddit_name)
+        response = requests.get(url, headers=header, allow_redirects=False)
+        childrens = response.json().get('data').get('children')
+        for children in childrens:
+            print(children.get('data').get('title'))
+    except Exception:
+        print(None)
